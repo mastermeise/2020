f14fa16d9f3229385b961ff41db274da97f3963c
mastermeise <mastermeise@gmail.com>
2020-03-09 22:17:24 -0400

    added links to JSON and sections to site

--- src/script.js
+++ src/script.js
@@ -1,25 +1,38 @@
 // ID of the Google Spreadsheet
 var spreadsheetID =
-  "1B88-yCOTs5Z2nxrfo71Ver7WXaITGkZ3t86lyFjkBFI";
- 
-// Make sure it is public or set to Anyone with link can view
-var url =
-  "https://spreadsheets.google.com/feeds/list/" +
-  spreadsheetID +
-  "/2/public/values?alt=json";
+    "1B88-yCOTs5Z2nxrfo71Ver7WXaITGkZ3t86lyFjkBFI";
 
-$.getJSON(url, function(data) {
-  
-  var entry = data.feed.entry;
-  
-  // Giving a link to the raw JSON for reference.
-  $(".json").html(
-    '<a href="' + url + ' "target="_blank">Link to source JSON</a>'
-  );
-  
-  $(entry).each(function(){
-    // Pulling in each entry
-    $('.books').prepend('<figure class="book"><h2 class="title">'+this.gsx$title.$t+'</h2><img src="'+this.gsx$img.$t+'" alt=""><span class="score">'+this.gsx$score.$t+'</span><span class="date">'+this.gsx$date.$t+'</span></figure>');
-  });
-  
+// These are variables for each of the tabs in the Google Sheets document
+var url1 =
+    "https://spreadsheets.google.com/feeds/list/" +
+    spreadsheetID +
+    "/1/public/values?alt=json";
+
+var url2 =
+    "https://spreadsheets.google.com/feeds/list/" +
+    spreadsheetID +
+    "/2/public/values?alt=json";
+
+var url3 =
+    "https://spreadsheets.google.com/feeds/list/" +
+    spreadsheetID +
+    "/3/public/values?alt=json";
+
+// Giving a link to the raw JSON for reference.
+$(document).ready(function() {
+
+    $(".json").html(
+        '<a href="' + url1 + ' "target="_blank">The Chain</a><a href="' + url2 + ' "target="_blank">Progress</a><a href="' + url3 + ' "target="_blank">Books Read</a>'
+    );
+});
+
+//Displaying Books Read
+$.getJSON(url3, function(data) {
+
+    var bookentry = data.feed.entry;
+
+    $(bookentry).each(function() {
+        // Pulling in each entry
+        $('.books').prepend('<figure class="book"><span class="title">' + this.gsx$title.$t + '</span><img src="' + this.gsx$img.$t + '" alt=""><span class="score">' + this.gsx$score.$t + '</span><span class="date">' + this.gsx$date.$t + '</span></figure>');
+    });
 });
